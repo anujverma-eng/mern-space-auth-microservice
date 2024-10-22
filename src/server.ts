e49da480aa1d67a43469b9acb07cf3e1f84e6717
anujverma-eng <anujverma123ok@gmail.com>
2024-10-22 06:18:41 +0530

    husky lint-staged

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,3 @@
-function login(userName: string) {
-  console.log("login");
+function login(userName: string) {console.log("login");
   return userName;
-}
-
-login("John Doe");
+}login("John Doe");
